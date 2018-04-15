0a1e9dbade13f39bdceea3b987aba2c645a5593a
jlwcrews <jlwcrews@gmail.com>
2018-04-15 18:11:56 +0200

    Network database

--- src/jlwcrews/flaggameserver/FlagGameServerController.java
+++ src/jlwcrews/flaggameserver/FlagGameServerController.java
@@ -33,51 +33,6 @@ public class FlagGameServerController implements Initializable{
         serverThread.start();
     }
 
-    //set up the database connection
-    private Connection dbConnect() {
-        String url = "jdbc:sqlite:flags.db";
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(url);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return conn;
-    }
-
-    //population the flags array with the appropriate flags based on the selected game mode
-    public void returnFlags(String gameMode) {
-        String sql = "select country, image from flags f " +
-                "join flag_groups fg on f.flag_id=fg.flag_id " +
-                "join groups g on g.group_id=fg.group_id ";
-        switch (gameMode) {
-            case "easy":
-                sql = sql + "where g.group_id=2;";
-                break;
-            case "medium":
-                sql = sql + "where g.group_id=3;";
-                break;
-            case "hard":
-                sql = sql + "where g.group_id=1;";
-                break;
-        }
-
-        try (Connection conn = this.dbConnect();
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) {
-
-            // loop through the result set
-            while (rs.next()) {
-                Flag flag = new Flag();
-                flag.setCountry(rs.getString("country"));
-                flag.setFlag(rs.getString("image"));
-                flags.add(flag);
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-
-    }
     public void showMessage(String message) {
         messageTextArea.appendText(message + "\n");
 
